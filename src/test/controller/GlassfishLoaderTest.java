2099fdd160678f06ad0a639ecabe927c1512edd6
hiept <trieuhiepptit@gmail.com>
2018-01-25 10:37:43 +0700

    build jar file

---
+++ src/test/controller/GlassfishLoaderTest.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GlassfishLoaderTest {
+    private static MainController.GlassfishLoader glassfishLoader;
+
+    @BeforeClass
+    public static void initAllTest() {
+    }
+
+    @Test
+    public void actionPerformed() throws Exception {
+    }
+
+}
